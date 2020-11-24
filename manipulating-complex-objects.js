7a29a9f26b09544b125a208e2255432fde3a2aae
SobiaF <25015478+SobiaF@users.noreply.github.com>
2020-11-24 23:35:31 +0000

    Add files via upload

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+var myMusic = [
+  {
+    "artist": "Ateez",
+    "title": "Fever",
+    "release_year": 2020,
+    "formats": [
+      "Albums",
+      "Spotify",
+      "Online"
+    ],
+    "Ultimate groups": true
+  },
+  {
+    "artist": "Bts",
+    "title": "Dynamite",
+    "release_year": 2020,
+    "formats": [
+      "Albums",
+      "Spotify",
+      "Online"
+    ]
+  }
+];
